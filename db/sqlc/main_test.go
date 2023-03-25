81a5ef9a4e99fdc580a0e50f82f7dedd0dc24897
RyotaroSeto <ryotaroseto@RyotaroSetonoMacBook-Pro.local>
2023-03-25 23:29:28 +0900

    envで環境変数化

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -4,22 +4,22 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"ryotarobank/db/util"
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	DBDriver = "postgres"
-	DBSource = "postgresql://postgres:postgres@localhost:5432/ryotaro_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(DBDriver, DBSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal(err)
 	}
